da31556dfe46b87250ff7eae5d8daedb4c8e4059
Ragvax <ragvaxofficial@gmail.com>
2021-03-28 18:31:37 +0800

    Refactor profile screen user interface

--- app/src/main/java/com/ragvax/picttr/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/ragvax/picttr/ui/profile/ProfileFragment.kt
@@ -5,7 +5,9 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.ragvax.picttr.R
+import com.ragvax.picttr.data.user.model.User
 import com.ragvax.picttr.databinding.FragmentProfileBinding
+import com.ragvax.picttr.utils.hide
 import com.ragvax.picttr.utils.loadProfilePicture
 import com.ragvax.picttr.utils.toPrettyString
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,11 +23,15 @@ class ProfileFragment : Fragment(R.layout.fragment_profile) {
         _binding = FragmentProfileBinding.bind(view)
         val user = args.user
 
+        initView(user)
+    }
+
+    private fun initView(user: User) {
         binding.apply {
             ivProfilePicture.loadProfilePicture(user)
             tvName.text = user.name
-            tvLocation.text = user.location
-            tvBio.text = user.bio
+            if (user.location != null) tvLocation.text = user.location else tvLocation.hide()
+            if (user.bio != null) tvBio.text = user.bio else tvBio.hide()
             tvItemPhotos.text = (user.total_photos ?: 0).toPrettyString()
             tvItemLikes.text = (user.total_likes ?: 0).toPrettyString()
             tvItemCollection.text = (user.total_collections ?: 0).toPrettyString()
